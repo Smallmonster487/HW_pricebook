3ed4e69a5389c8fbec150bf0173154084ef2fdb6
DESKTOP-9MF0OK7\小怪 <s11228246@gmail.com>
2018-06-16 00:44:44 +0800

    第一次上傳  刪除事件未完成

---
+++ HW_pricebook/HW_pricebook/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_pricebook
+{
+    /// <summary>
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            addprice item = new addprice();
+
+            TaskList.Children.Add(item);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            int AddPrice = 0;
+
+            //按enter時會計算
+            if (e.Key == Key.Return)
+            {
+
+                foreach (addprice item in TaskList.Children)
+                {
+
+                    AddPrice += item.itemPriceValue;
+                }
+
+                //顯示總價
+                totalPrice.Text = AddPrice.ToString();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+            //設置一個字串
+            string data = "";
+
+            // 轉換項目
+            foreach (addprice item in TaskList.Children)
+            {
+
+                //每一種資料區隔加入字串
+                data += item.Daytimes + "|" + item.ItemName + "|" + item.ItemPrice + "\r\n";
+
+            }
+            //存檔
+            System.IO.File.WriteAllText(@"D:\data.txt", data);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 開啟檔案
+            string[] lines = System.IO.File.ReadAllLines(@"D:\data.txt");
+
+            // 分析
+            foreach (string line in lines)
+            {
+                // 隔開
+                string[] parts = line.Split('|');
+
+                //建立元件
+                addprice item = new addprice();
+
+                //讀取
+                item.Daytimes = parts[0];
+                item.ItemName = parts[1];
+                item.ItemPrice = parts[2];
+
+                //增加項目
+                TaskList.Children.Add(item);
+            }
+        }
+
+        private void AddButten_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            int AddPrice = 0;
+
+            foreach (addprice item in TaskList.Children)
+                {
+
+                    AddPrice += item.itemPriceValue;
+                }
+
+              //顯示總價
+              totalPrice.Text = AddPrice.ToString();
+           
+        }
+    }
+}
---
+++ HW_pricebook/HW_pricebook/addprice.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_pricebook
+{
+    /// <summary>
+    /// price.xaml 的互動邏輯
+    /// </summary>
+    
+    
+
+    public partial class addprice : UserControl
+    {
+
+        public string Daytimes
+        {
+            get
+            {
+                return DayTime.Text;
+            }
+            set
+            {
+                DayTime.Text = value;
+            }
+
+        }
+        public string ItemName
+        {
+            get
+            {
+                return TaskName.Text;
+            }
+            set
+            {
+                TaskName.Text = value;
+            }
+
+        }
+        public string ItemPrice
+        {
+            get
+            {
+                return TaskPrice.Text;
+            }
+            set
+            {
+                TaskPrice.Text = value;
+            }
+
+        }
+        public addprice()
+        {
+            InitializeComponent();
+        }
+        // 自訂刪除事件
+        public event EventHandler DeleteItem;
+
+        // 項目名稱鍵盤按下事件
+        private void TaskNamePreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // 任務空白，而且按下 Backspace 鍵時，引發 DeleteItem 事件
+            if (TaskName.Text == "" && e.Key == Key.Back)
+            {
+                DeleteItem(this, null);
+            }
+        }
+        public int itemPriceValue
+        {
+            get
+            {
+                //嘗試解析價格
+                try
+                {
+                    return int.Parse(TaskPrice.Text);
+                }
+                //失敗後要求用家輸入數字
+                catch
+                {
+                    MessageBox.Show("請輸入數字");
+                    return 0;
+                }
+            }
+            set
+            {
+                TaskPrice.Text = value.ToString();
+            }
+        }
+       
+
+    }
+    
+}
